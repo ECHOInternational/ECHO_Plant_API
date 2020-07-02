9f91657072c07a5651fd83cb049c1d0b0840edad
Nate Flood <nflood@echonet.org>
2020-07-02 15:10:41 -0400

    Ported policies and controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,23 @@
 class ApplicationController < ActionController::API
-	include Pundit
-	before_action :set_paper_trail_whodunnit
+  include ActionController::HttpAuthentication::Token::ControllerMethods
+  before_action :require_token
+  before_action :set_paper_trail_whodunnit
+
+  def current_user
+  	@current_user
+  end
+
+  private
+
+  def require_token
+  #   authenticate_or_request_with_http_token do |token|
+  #     begin
+  #       jwt_payload = JWT.decode(token, ENV['APPLICATION_JWT_SECRET'], true, { algorithm: ENV['APPLICATION_JWT_ALGORITHM']})
+  #       @current_user = User.new(jwt_payload[0]["user"])
+  #     rescue
+  #       @current_user = nil
+  #     end
+  #   end
+    @current_user = User.new({'uid' => 'test', 'email' => 'test@test.com', 'trust_levels' => {'plant' => 9}});
+  end
 end
--- app/graphql/plant_api_schema.rb
+++ app/graphql/plant_api_schema.rb
@@ -16,23 +16,33 @@ class PlantApiSchema < GraphQL::Schema
     # Here's a simple implementation which:
     # - joins the type name & object.id
     # - encodes it with base64:
-    # GraphQL::Schema::UniqueWithinType.encode(type_definition.name, object.id)
+    GraphQL::Schema::UniqueWithinType.encode(type_definition.name, object.id)
   end
 
   # Given a string UUID, find the object
   def self.object_from_id(id, query_ctx)
     # For example, to decode the UUIDs generated above:
-    # type_name, item_id = GraphQL::Schema::UniqueWithinType.decode(id)
+    type_name, item_id = GraphQL::Schema::UniqueWithinType.decode(id)
     #
     # Then, based on `type_name` and `id`
     # find an object in your application
-    # ...
+    klass = type_name.constantize
+    klass.find item_id
   end
 
   # Object Resolution
   def self.resolve_type(type, obj, ctx)
-    # TODO: Implement this function
-    # to return the correct type for `obj`
-    raise(GraphQL::RequiredImplementationMissingError)
+    case obj
+    when Category
+      Types::CategoryType
+    when Image
+      Types::ImageType
+    when ImageAttribute
+      Types::ImageAttribute
+    when Upload
+      Types::Upload
+    else
+      raise("Unexpected object: #{obj}")
+    end
   end
 end
