cb59f6c8f0b3ccf66d571e0161c4ca3eb039b1bc
Nate Flood <nflood@echonet.org>
2020-07-31 20:47:37 -0400

    Implemented Category mutation: delete

---
+++ app/graphql/mutations/delete_category.rb
@@ -0,0 +1,27 @@
+module Mutations
+	class DeleteCategory < BaseMutation
+		argument :category_id, ID, required: true, loads: Types::CategoryType
+
+		field :category_id, ID, null: true
+		field :errors, [String], null: false
+
+		def authorized?(category:, **attributes)
+			authorize category, :destroy?
+		end
+
+		def resolve(category:, **attributes)
+			id = category.id
+			if category.destroy
+				{
+					category_id: id,
+					errors: []
+				}
+			else
+				{
+					category_id: nil,
+					errors: category.errors.full_messages
+				}
+			end
+		end
+	end
+end
--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -2,5 +2,6 @@ module Types
   class MutationType < Types::BaseObject
     field :create_category, mutation: Mutations::CreateCategory, description: "Creates a new plant category"
     field :update_category, mutation: Mutations::UpdateCategory, description: "Updates a plant category"
+    field :delete_category, mutation: Mutations::DeleteCategory, description: "Deletes a plant category"
   end
 end
